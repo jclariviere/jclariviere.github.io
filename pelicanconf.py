48b0b1db34fa4d7bd7f9e3ed05e26aa545a6c357
J-C Lariviere <jclariviere2@gmail.com>
2015-10-14 02:47:34 +0000

    Url settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,5 +34,10 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
+# Date and slug in filename
 FILENAME_METADATA = '(?P<date>\d{4}-\d{2}-\d{2})-(?P<slug>.*)'
 SLUGIFY_SOURCE = 'basename'
+
+# Url settings
+ARTICLE_URL = 'posts/{slug}'
+ARTICLE_SAVE_AS = 'posts/{slug}/index.html'
