8088c28cde5c379c9b4c7dd1db4271605236649b
J-C Lariviere <jclariviere2@gmail.com>
2015-10-15 01:43:24 +0000

    Add social links

--- pelicanconf.py
+++ pelicanconf.py
@@ -22,14 +22,15 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('github', 'https://github.com/jclariviere'),
+          ('twitter', 'https://twitter.com/jclariviere'),
+          ('linkedin', 'https://www.linkedin.com/in/jclariviere'))
 
 DEFAULT_PAGINATION = 10
 
