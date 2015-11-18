0101fea5cd39465d546ea32d0c48c5693bea4793
J-C Lariviere <jclariviere2@gmail.com>
2015-11-18 05:42:40 +0000

    Setup sidebar

---
+++ README.md
@@ -0,0 +1,13 @@
+# Setup
+
+## Plugins
+First get:
+```
+git clone --recursive https://github.com/getpelican/pelican-plugins /pelican/pelican-plugins
+```
+
+Update:
+```
+cd /pelican/pelican-plugins
+git submodule update --init --recursive
+```
--- pelicanconf.py
+++ pelicanconf.py
@@ -14,6 +14,9 @@ DEFAULT_LANG = u'en'
 
 THEME = 'pelican-bootstrap3'
 
+PLUGIN_PATHS = ['/pelican/pelican-plugins']
+PLUGINS = ['tag_cloud']
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -31,6 +34,16 @@ AUTHOR_FEED_RSS = None
 SOCIAL = (('github', 'https://github.com/jclariviere'),
           ('twitter', 'https://twitter.com/jclariviere'),
           ('linkedin', 'https://www.linkedin.com/in/jclariviere'))
+
+DISPLAY_CATEGORIES_ON_SIDEBAR = True
+CATEGORIES_URL = 'categories.html'
+
+DISPLAY_TAGS_ON_SIDEBAR = True
+#DISPLAY_TAGS_INLINE = True
+TAGS_URL = 'tags.html'
+TAG_CLOUD_STEPS = 2
+TAG_CLOUD_SORTING = 'alphabetically'
+
 
 DEFAULT_PAGINATION = 10
 
